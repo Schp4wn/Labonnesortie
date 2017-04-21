d1f78c83b5ec53f509098cd6e6c16bc958458a3f
sa.dev <sa.rg33@yahoo.fr>
2017-04-21 17:26:39 +0200

    pull origin master

--- app/Controller/SecurityController.php
+++ app/Controller/SecurityController.php
@@ -61,4 +61,39 @@ class SecurityController extends Controller
     }
 
     
+
+    /**
+     * Permet la connexion d'un utilisateur
+    */
+    public function login()
+    {
+        if (!empty($_POST)) {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $auth_manager = new \W\Security\AuthentificationModel();
+
+            $user_id = $auth_manager->isValidLoginInfo($username, $password);
+            if ($user_id) { // Si le couple username/password est valid
+                $user_manager = new UserModel();
+                $user = $user_manager->find($user_id); // Récupére toutes les infos de l'utilisateur qui se connecte
+                $auth_manager->logUserIn($user); // La connexion se fait
+                $this->redirectToRoute('default_home');
+            }
+        }
+
+        //var_dump($this->getUser());
+
+        $this->show('security/login');
+    }
+
+    /**
+     * Déconnexion de l'utilisateur
+    */
+    public function logout()
+    {
+        $auth_manager = new \W\Security\AuthentificationModel();
+        $auth_manager->logUserOut(); // Déconnecte l'utilisateur connecté
+        $this->redirectToRoute('default_home');
+    }
 }
+
--- app/routes.php
+++ app/routes.php
@@ -1,9 +1,30 @@
 <?php
-	
+
 	$w_routes = array(
-		//accueil
-		['GET|POST'      , '/'         , 'Default#home', 'default_home'],
-		
-		['GET|POST' , '/register'  ,'Security#register', 'security_register'], //inscription
-		
+
+		['GET', '/', 'Default#home', 'default_home'],//page d'acceuil
+
+		//Dans le fichier je Crée le controller ContactController
+		['GET|POST', '/contact', 'Default#contact', 'default_contact'], //affiche la page contact
+
+		//Dans le fichier je Crée le controller SecurityController
+		['GET|POST', '/login', 'Security#login', 'security_login'], //connexion
+		['GET|POST', '/register', 'Security#register', 'security_register'], //inscription
+		['GET|POST', '/logout', 'Security#logout', 'security_logout'], //deconnexion
+
+		//Dans le fichier je Crée le controller EventController
+		['GET|POST', '/event', 'Event#index', 'event_index'], //afficher un evenement
+		['GET|POST', '/event/create', 'Event#create', 'event_create'], //add un evenement
+		['GET|POST', '/event/update', 'Event#update', 'event_update'], //modifie un evenement
+		['GET|POST', '/event/delete', 'Event#delete', 'event_delete'], //supprime un evenement
+
+		//Dans le fichier je Crée le controller WayController
+		['GET|POST', '/way', 'Way#index', 'way_index'], //afficher un trajet
+		['GET|POST', '/way/create', 'Way#create', 'way_create'], //add un trajet
+		['GET|POST', '/way/update', 'Way#update', 'way_update'], //modifie un trajet
+		['GET|POST', '/way/delete', 'Way#delete', 'way_delete'], //supprime un trajet
+
+
+		//Dans le fichier je Crée le controller ProfilController
+		['GET|POST', '/profil', 'Profil#index', 'profil_index'], //affiche la page profil
 	);
