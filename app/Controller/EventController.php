c21957a5fe7f140785a8f897513fcc14fd2072a0
anibalsoul <mohammed.eat@gmail.com>
2017-04-24 16:55:33 +0200

    update

--- app/Controller/EventController.php
+++ app/Controller/EventController.php
@@ -7,15 +7,20 @@ use  Model\EventsModel;
 
 
 class EventController extends Controller
-{   
+{
 
     /**
       *  On creer un evenements
       *
      **/
     public function create()
-    {   
-        $this->allow(['admin' , 'user']);  //les inscrit en tant qu'admin seront les seuls a ajouter des events
+
+
+    {
+        //$this->allow('admin');
+
+
+
 
         $title   = null ;
         $event   = null ;
@@ -69,28 +74,88 @@ class EventController extends Controller
     }
 
     /**
-      *  Recupère un seul évènement   
+      *  Recupère un seul évènement
       *
      **/
     public function view($id)
-    {   
-        $this->allow(['admin' , 'user']);
 
-        $event_manager = new EventsModel();  
+    {
+        $event_manager = new EventsModel();
+
         $event = $event_manager->find($id);
         $this->show('event/view' , ['event'=> $event]);
     }
 
     /**
-      *  Recupère tous les évènement   
+      *  Recupère tous les évènement
       *
      **/
     public function index()
-    {       
+    {
         $this->allow(['admin' , 'user']);
 
         $event_manager = new EventsModel();
         $events =  $event_manager->findAll();
         $this->show('event/index' , ['events' => $events]);
+    }
+
+    /**
+     * Edition d'un article
+    */
+    public function update($id)
+    {
+        $title   = null ;
+        $event   = null ;
+        $date    = null ;
+        $message = null ;
+        $this->allowTo('admin');
+        $this->allowTo('user');
+
+
+        $event = $event_manager->find($id); // Je vais chercher un evenement dans la bdd par son id
+
+        if (!empty($_POST)) {
+          $title = trim($_POST['title']);
+          $event = trim($_POST['event']);
+          $date  = date('Y-m-d H:i:s' , strtotime( $_POST['date'] ));
+          $event_manager = new EventsModel();
+
+          $errors=[];
+
+          if( strlen($title) < 3 && !empty($title) )
+          {
+              $errors['title'] = "Le titre doit comporter 3 caractères minimum.";
+          }
+
+          if( strlen($event) < 15 && !empty($title))
+          {
+              $errors['event'] = "Votre paragraphes doit comporte 15 lignes minimum.";
+          }
+          if(!is_numeric(strtotime($_POST['date']) )  && !empty($date))
+          {
+              $errors['date']= "Votre date doit etre au format Année/Mois/Jours .";
+          }
+
+          if( empty($error) )
+          {
+              $auth_manager = new \W\Security\AuthentificationModel();
+
+         $result = $event_manager->insert([
+                 'title'     => $title,
+                 'event'     => $event,
+                 'date_time' => $date,
+                 'user_id'   => $this->getUser()['id']  // ici l'id de lutilisateur connecté $this->getuser()['id']
+               ]);
+
+              // var_dump($result);
+
+               $message = ["L'evenement a bien etait enregistré"];
+          }
+          else{
+              $message = $errors ;
+          }
+        }
+
+        $this->show('event/update' , ['message' => $message  , 'title'=>$title , 'event' => $event ]);
     }
 }
--- app/Model/UserModel.php
+++ app/Model/UserModel.php
@@ -16,7 +16,7 @@ class UserModel extends UsersModel
 }
 
 function changeUserPassword($id, $password) {
-    global $db;
+
     $query = $this->dbh->prepare('UPDATE users SET password = :password, token_forget = NULL, date_forget = NULL WHERE id = :id'); // On met à jour le mot de passe de l'utilisateur et on supprime le token
     $query->bindValue(':id', $id);
     $query->bindValue(':password', password_hash($password, PASSWORD_DEFAULT));
@@ -24,9 +24,56 @@ function changeUserPassword($id, $password) {
 }
 
 function changeTokenLogin($user_id) {
-    global $db;
+
     $token_login = sha1(md5(sha1($user_id) . sha1(time()) . md5('1a4g51rz74hz21rz4h') . md5(uniqid()))); // Génére un token du style 3a4f74a7f5a7f4v7g4ae5g41ae2gea87gv
     $db->query('UPDATE users SET token_login = "'.$token_login.'" WHERE id = ' . $user_id);
     return $token_login;
+}
+
+function checkUserByEmail($user_email) {
+
+    $query = $this->dbh->prepare('SELECT id FROM user WHERE email = :email');
+    $query->bindValue(':email', $user_email, PDO::PARAM_STR);
+    $query->execute();
+    return $query->fetchColumn();
+}
+
+function checkUser($login, $password) {
+
+    $query = $this->dbh->prepare('SELECT COUNT(*) as count, id, login, email, password, role, registered_at FROM user WHERE login = :login');
+    $query->bindValue(':login', $login, PDO::PARAM_STR);
+    $query->execute();
+    $user = $query->fetch(); // Je stocke le résultat de ma requête dans $user
+    if ($user['count']) {
+        // Vérification du mot de passe
+        if (password_verify($password, $user['password'])) { // Je compare le mot de passe en clair avec le mot de passe hashé
+            return $user;
+        } else {
+            return 'Le mot de passe n\'est pas correct';
+        }
+    } else {
+        return 'L\'utilisateur n\'existe pas';
+    }
+}
+
+function userExists($email, $login) {
+    global $db;
+    // On fait une requête pour vérifier si l'utilisateur existe ou pas
+    $query = $this->dbh->prepare('SELECT COUNT(*) FROM user WHERE email = :email OR login = :login');
+    $query->bindValue(':email', $email, PDO::PARAM_STR);
+    $query->bindValue(':login', $login, PDO::PARAM_STR);
+    $query->execute();
+    return $query->fetchColumn(); // Return 0, 1 ou 2, fetchColumn retourne la valeur de la première colonne
+}
+
+function registerUser($login, $email, $password) {
+    global $db;
+    $query = $this->dbh->prepare("INSERT INTO user (login, email, password, role, registered_at) VALUES (:login, :email, :password, 'customer', NOW())");
+    $query->bindValue(':login', $login);
+    $query->bindValue(':email', $email);
+    $query->bindValue(':password', password_hash($password, PASSWORD_DEFAULT));
+    if ($query->execute()) {
+        return $db->lastInsertId();
+    }
 }
 }
