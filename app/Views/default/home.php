2ec5d1bfb7f7f95a86ba87d762389be9dd4a2b1a
boulouh <mustapha.boulouh@icloud.com>
2017-04-24 13:47:49 +0200

    accueil

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -2,9 +2,87 @@
 
 <?php $this->start('main_content') ?>
 
-	<h2>Let's code.</h2>
-	<p>Vous avez atteint la page d'accueil. Bravo.</p>
-	
-	<p>Et maintenant, RTFM dans <strong><a href="../docs/tuto/" title="Documentation de W">docs/tuto</a></strong>.</p>
-<?php $this->stop('main_content') ?>
 
+		<div class="demo-header">
+				<div class="demo-header-content">
+						<div class="container">
+								<h1>Bienvenu sur LaBonneSortie !</h1>
+								<hr class="colored">
+								<p>Nous  toutes les sortie organiser dans le nord de la france !</p>
+								<ul class="list-inline cta-list">
+										<li class="list-inline-item">
+												<a href="#previews" class="btn btn-primary page-scroll-demo">Nord</a>
+										</li>
+										<li class="list-inline-item">
+												<a href="#previews2" class="btn btn-primary page-scroll-demo">Pas de calais</a>
+										</li>
+										<li class="list-inline-item">
+												<a href="docs/index.html" class="btn btn-primary page-scroll-demo">Plus d'infos</a>
+										</li>
+								</ul>
+						</div>
+				</div>
+		</div>
+		<div class="container pt-2" id="previews">
+				<div class="row">
+						<div class="col-md-6">
+			 <script type="text/javascript"><!--
+						var lion_commune=new Array();
+						var lion_dpt_couleur_fond=new Array();
+						lion_image_bordure=0;
+						lion_taille_image=600;
+						lion_carte="59";
+						lion_carte="62";
+						lion_carte_couleur_fond="ffffff";
+						lion_carte_couleur="ccccff";
+						lion_carte_couleur_contour="0000FF";
+						lion_carte_epaisseur_contour="3";
+						lion_affiche_communes="oui";
+						lion_affiche_communes_couleur="666666";
+						lion_affiche_communes_contour="1";
+						lion_affiche_dpts_voisins="non";
+						lion_couleur_dpts_voisins="222222";
+						lion_communes_surbrillance="ffff00";
+
+						//--></script>
+						<script type="text/javascript"
+						src="http://www.lion1906.com/outils/outils.js">
+						</script>
+								</div>
+						</div>
+						<div class="container pt-2" id="previews2">
+				<div class="row">
+						<div class="col-md-6">
+			 <script type="text/javascript"><!--
+						var lion_commune=new Array();
+						var lion_dpt_couleur_fond=new Array();
+						lion_image_bordure=0;
+						lion_taille_image=600;
+						lion_carte="59";
+						lion_carte_couleur_fond="ffffff";
+						lion_carte_couleur="ccccff";
+						lion_carte_couleur_contour="0000FF";
+						lion_carte_epaisseur_contour="3";
+						lion_affiche_communes="oui";
+						lion_affiche_communes_couleur="666666";
+						lion_affiche_communes_contour="1";
+						lion_affiche_dpts_voisins="non";
+						lion_couleur_dpts_voisins="222222";
+						lion_communes_surbrillance="ffff00";
+
+						//--></script>
+						<script type="text/javascript"
+						src="http://www.lion1906.com/outils/outils.js">
+						</script>
+								</div>
+						</div>
+					 </div>
+
+		<!-- Core Scripts -->
+
+
+		<!-- Plugin Scripts -->
+
+		<!-- Vitality Theme Scripts -->
+
+<?php $this->stop('main_content') ?>
