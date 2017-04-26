701e715f56dd0a9a4b33c114ea6e312d215f0ce3
rorschach59 <remi.pouly@free.fr>
2017-04-26 14:24:12 +0200

    refonte de la frontpage

--- app/Views/default/frontPage.php
+++ app/Views/default/frontPage.php
@@ -3,91 +3,46 @@
 <?php $this->start('main_content') ?>
 
 <div id="frontPage">
-  <div class="row">
-
-
-  <div class="col-lg-3 meteo">
-    <div id="cont_NTkzNTB8NXwxfDJ8MXxCQkUwRkZ8M3xGRkZGRkZ8Y3wx">
-      <div id="spa_NTkzNTB8NXwxfDJ8MXxCQkUwRkZ8M3xGRkZGRkZ8Y3wx">
-        <a id="a_NTkzNTB8NXwxfDJ8MXxCQkUwRkZ8M3xGRkZGRkZ8Y3wx" href="http://www.meteocity.com/france/lille_v59350/" target="_blank" style="color:#333;text-decoration:none;">Météo Lille</a> ©<a href="http://www.meteocity.com">meteocity.com</a></div>
-        <script type="text/javascript" src="http://widget.meteocity.com/js/NTkzNTB8NXwxfDJ8MXxCQkUwRkZ8M3xGRkZGRkZ8Y3wx"></script>
-      </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div id="jssor_1" style="position:relative;margin:0 auto;top:0px;left:0px;width:1300px;height:500px;overflow:hidden;visibility:hidden;">
+        <!-- Loading Screen -->
+        <div data-u="loading" style="position:absolute;top:0px;left:0px;background-color:rgba(0,0,0,0.7);">
+          <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
+          <div style="position:absolute;display:block;background:url('<?= $this->assetUrl('img/slider/loading.gif') ?>') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
+        </div>
+        <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:1300px;height:500px;overflow:hidden;">
+          <div>
+            <!-- First slider -->
+            <img data-u="image" src="<?= $this->assetUrl('img/slider/create-event.jpg') ?>" />
+            <div style="position:absolute;top:30px;left:30px;width:480px;height:120px;z-index:0;font-size:50px;color:#ffffff;line-height:60px;">CREEZ VOS EVENEMENTS</div>
+            <div style="position:absolute;top:300px;right:30px;width:480px;height:120px;z-index:0;font-size:30px;color:#ffffff;line-height:38px;">Vous avez la possibilité de créez vos événements ainsi que de retrouver tous ceux auxquels vous participez</div>
+          </div>
+            <div>
+              <!-- Second slider -->
+              <img data-u="image" src="<?= $this->assetUrl('img/slider/googlemap.png') ?>" />
+              <div style="position:absolute;top:30px;left:30px;width:480px;height:120px;z-index:0;font-size:50px;color:rgb(113, 113, 113);line-height:60px;">PARTAGEZ VOS TRAJETS</div>
+              <div style="position:absolute;top:300px;right:30px;width:480px;height:120px;z-index:0;font-size:30px;color:rgb(113, 113, 113);line-height:38px;">Vous connaissez un parcours qui mérite d'être connu, venez le partager et découvrez un tas d'autres parcours</div>
+            </div>
+            <div>
+              <!-- Third slider -->
+              <img data-u="image" src="<?= $this->assetUrl('img/slider/blue.jpg') ?>" />
+              <div style="position:absolute;top:30px;left:30px;width:480px;height:120px;z-index:0;font-size:50px;color:#ffffff;line-height:60px;">CONSULTEZ LA METEO</div>
+              <div style="position:absolute;top:300px;right:30px;width:480px;height:120px;z-index:0;font-size:30px;color:#ffffff;line-height:38px;">Pour ne plus être surpris par le temps capricieux de notre région, consultez la météo de chaque jour</div>
+            </div>
+        </div>
+        <!-- Bullet Navigator -->
+        <div data-u="navigator" class="jssorb05" style="bottom:16px;right:16px;" data-autocenter="1">
+          <!-- bullet navigator item prototype -->
+          <div data-u="prototype" style="width:16px;height:16px;"></div>
+        </div>
+      <!-- Arrow Navigator -->
+      <span data-u="arrowleft" class="jssora22l" style="top:0px;left:8px;width:40px;height:58px;" data-autocenter="2"></span>
+      <span data-u="arrowright" class="jssora22r" style="top:0px;right:8px;width:40px;height:58px;" data-autocenter="2"></span>
     </div>
 
-  <!-- <div class="col-lg-2">
-    <iframe name="InlineFrame1" id="InlineFrame1" style="width:860px;height:235px;" src="http://www.mathieuweb.fr/calendrier/calendrier-des-semaines.php?nb_mois=1&nb_mois_ligne=5&mois=0&an=0&langue=fr&texte_color=B9CBDD&week_color=DAE9F8&week_end_color=C7DAED&police_color=453413&sel=true" scrolling="no" frameborder="0" allowtransparency="true"></iframe>
-  </div> -->
-
-  <h3 class="title">Vous cherchez les événements autour de Lille</h3>
-      <div class="col-lg-3 col-lg-push-1 event">
-        <h4 class="text-center">Evénement à venir</h4>
-        <div class="col-lg-12 adress-event text-center">
-          <h4>Moto show</h4>
-          <ul class="list-unstyled">
-            <i class="fa fa-map-marker" aria-hidden="true"></i>
-            <li>104 avenue de la République</li>
-            <li>59110</li>
-            <li>La Madeleine</li>
-          </ul>
-          <p><i class="fa fa-calendar-o" aria-hidden="true"></i> Samedi 08 avril 2017</p>
-          <img class="img-responsive center-block" src="<?= $this->assetUrl('img/tampon_evenement.png')?>" alt="event">
-        </div>
-          <p class="text-center">Description de l'événement</p>
-      </div>
-
-      <div class="col-lg-3 col-lg-push-3 tchat">
-        <div class="thumbnail">
-          <div class="caption">
-            <h3 class="text-center">Postez votre message</h3>
-            <hr>
-            <ul class="message-from-tchat">
-              <?php foreach ($messages as $message): ?>
-                <?= "<li>" .$message['content']."</li>"; ?>
-              <?php endforeach; ?>
-              <li>Message 1</li>
-              <li>Message 2</li>
-              <li>Message 3</li>
-              <li>Message 4</li>
-              <li>Message 5</li>
-              <li>Message 6</li>
-              <li>Message 7</li>
-            </ul>
-            <form id="addMessage" action="" method="post">
-              <div class="input-group">
-                <span class="input-group-btn">
-                  <textarea name="message" class="form-control" rows="5"></textarea>
-                  <button class="btn btn-primary form-control" type="submit">Envoyer</button>
-                </span>
-              </div><!-- /input-group -->
-            </form>
-          </div>
-        </div>
-      </div><!-- /.tchat -->
     </div><!-- /.row -->
-
-    <div class="row">
-      <div class="col-lg-2 calendar">
-        <img src="<?= $this->assetUrl('img/calendar.png')?>" alt="calendar">
-
-      </div>
-      <div class="col-lg-3 col-lg-push-2 event">
-        <h4 class="text-center">Evénement à venir</h4>
-        <div class="col-lg-12 adress-event text-center">
-          <h4>Moto show</h4>
-          <ul class="list-unstyled">
-            <i class="fa fa-map-marker" aria-hidden="true"></i>
-            <li>104 avenue de la République</li>
-            <li>59110</li>
-            <li>La Madeleine</li>
-          </ul>
-          <p><i class="fa fa-calendar-o" aria-hidden="true"></i> Samedi 08 avril 2017</p>
-          <img class="img-responsive center-block" src="<?= $this->assetUrl('img/tampon_evenement.png')?>" alt="event">
-        </div>
-          <p class="text-center">Description de l'événement</p>
-      </div>
-
-    </div><!-- /.row -->
-
+  </div><!-- /.container-fluid -->
 </div><!-- /#frontPage -->
 
 <?php $this->stop('main_content') ?>
