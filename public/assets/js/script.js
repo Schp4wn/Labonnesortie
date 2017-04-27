cef87adcdf8f7c55d4d78d1edc61d606bdcbb186
rorschach59 <remi.pouly@free.fr>
2017-04-27 16:28:37 +0200

    16:28

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -74,4 +74,28 @@ window.onload = function () {
   })
 
   }); // $('#addMessage').submit(function(event)
+
+  var hoverSpan = $('.btn span');
+
+   $( ".btn" ).mouseenter(function() {
+       //add class enter on hover
+
+       $(hoverSpan).removeClass('reset').addClass('enter');
+
+     })
+     .mouseleave(function() {
+      //remove class enter
+       //add class leave and reset on complete
+
+       $(hoverSpan).removeClass('enter').addClass('leave');
+
+       setTimeout(function() {
+         $(hoverSpan).removeClass('leave').addClass('reset');
+       }, 500);
+
+     });
+
+
+
+
 } // window.onload
